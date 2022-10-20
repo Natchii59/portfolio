0e87ee47e5b7a138c221c640849f890af977870a
Natchii59 <caron.nathan@hotmail.com>
2022-10-20 14:06:14 +0200

    Fix uses page

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -40,24 +40,30 @@ const Uses: NextPage = () => {
           ))}
       </ul>
 
-      <h3 className='text-2xl mb-2'>Bientôt...</h3>
+      {uses.filter((u) => !u.has).length ? (
+        <>
+          <h3 className='text-2xl mb-2'>Bientôt...</h3>
 
-      <ul className='list-disc text-lg ml-5'>
-        {uses
-          .filter((u) => !u.has)
-          .map((u, i) => (
-            <li key={i}>
-              <a
-                href={u.url}
-                target='_blank'
-                rel='noreferrer'
-                className='link-underline'
-              >
-                {u.name}
-              </a>
-            </li>
-          ))}
-      </ul>
+          <ul className='list-disc text-lg ml-5'>
+            {uses
+              .filter((u) => !u.has)
+              .map((u, i) => (
+                <li key={i}>
+                  <a
+                    href={u.url}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='link-underline'
+                  >
+                    {u.name}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   )
 }
